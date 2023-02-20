f37115370532099f2c7b5ac8bf57992d30de462c
YouMustPh <you@example.comphps6200@gmail.com>
2023-02-20 13:30:48 -0300

    facebook verification

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,11 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import "@master/css";
 import { Analytics } from "@vercel/analytics/react";
-import { Helmet } from "react-helmet";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Helmet>
-        <meta
-          name="facebook-domain-verification"
-          content="48u1i5mrzdka54tpxve8kcpceme65a"
-        />
-      </Helmet>
+      {" "}
       <Component {...pageProps} />
       <Analytics />
     </>
