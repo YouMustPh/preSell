41458cad294c0af4b33640a3bf87702fc28bacff
YouMustPh <you@example.comphps6200@gmail.com>
2023-02-20 13:12:45 -0300

    feat: preSell

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import '@master/css';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import "@master/css";
+import { Analytics } from "@vercel/analytics/react";
+import { Helmet } from "react-helmet";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Helmet>
+        <meta
+          name="facebook-domain-verification"
+          content="48u1i5mrzdka54tpxve8kcpceme65a"
+        />
+      </Helmet>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,37 @@
 import type { NextPage } from "next";
-import Image from "next/image";
+import useWindowSize from "../utils/windowDimensions";
 
 const Home: NextPage = () => {
+  const { width, height } = useWindowSize();
+
   return (
-    <div className={`ai:center flex flex:col`}>
-      <h1 className={`f:100 f:red`}>Pedro Furukawa is my religion</h1>
-      <div className={`@rotate|2s|infinite`} onClick={
-        () => {
-          window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
-        }
-      }>
-        <Image
-          src="/pietro.jpg"
-          alt="Pedro Furukawa"
-          width={500}
-          height={500}
-        />
+    <div className={`h:${height} rel`}>
+      <div
+        className={`flex flex:col h:full p:64 jc:space-between ai:center bg:#ececec `}
+      >
+        <p
+          className={`color:black f:32 f:44@sm f:64@lg f:bold m:0 text:center`}
+        >
+          Deseja se livrar do patrão e trabalhar por conta própria para ter sua
+          independência financeira?
+        </p>
+        <div className={`w:100% w:80%@sm w:50%@lg flex ai:center jc:center`}>
+          <video
+            width="100%"
+            controls
+            src="/disccountvideo.mp4"
+            autoPlay
+            loop
+          />
+        </div>
+        <button
+          onClick={() => {
+            window.location.href = process.env.SELL_URI || "";
+          }}
+          className={`flex b:rgb(64,45,102)|2px|solid bg:linear-gradient(#0716eccf,#4d70e2) cursor:pointer opacity:0.8 opacity:1:hover h:56 h:72@sm w:520@sm w:100% r:999 ai:center jc:center f:20 f:32@sm f:bold f:#f2f2f2 `}
+        >
+          Saiba Mais
+        </button>
       </div>
     </div>
   );
